4ffc52e15ede9a62b6495e9d98e81abcc8724781
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-05 13:06:00 +0530

    Resizer fixed

--- support/resizeImg.py
+++ support/resizeImg.py
@@ -4,23 +4,24 @@ import os, sys
 path = r"D:\btwin\data"
 celebs = os.listdir( path )
 #print(celebs)
-
+resized = r"D:\btwin\data_resized"
 def resize():
     for celeb in celebs:
             a = 1
             path_celeb = os.path.join(path, celeb)
             dirs = os.listdir( path_celeb )
+            newpath = os.path.join(resized, celeb) 
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
             for item in dirs:
                 if os.path.isfile(os.path.join(path_celeb,item)):
                     im = Image.open(os.path.join(path_celeb,item))
-                    f, e = os.path.splitext(os.path.join(path_celeb,item))
-                    
-                    f = os.path.join(path, 'resized\\')
-                    print(f)
+                    f = os.path.join(resized, celeb)
+                    # print(f+"\\"+str(a) +'.jpg')
                     imResize = im.resize((200,200), Image.ANTIALIAS)
                     if imResize.mode != "RGB":
                         imResize = imResize.convert("RGB")
-                    imResize.save(f+ str(a) +'.jpg', 'JPEG', quality=90)
+                    imResize.save(f+"\\"+str(a) +'.jpg', 'JPEG', quality=90)
                     a += 1
 
 resize()
