e718c80a46698029073aaa9a18f8b2618d85fcd6
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-06 19:51:03 +0530

    Data collection done

--- support/classes.py
+++ support/classes.py
@@ -1,6 +1,8 @@
 import os
 a = open("classes.txt", "w")
+num = 1
 for path, subdirs, files in os.walk(r'D:\btwin\data_resized'):
    for subdir in subdirs:
-      a.write(subdir+"\n") 
+      a.write(str(num)+". "+subdir+"<br>"+"\n") 
+      num += 1
       
